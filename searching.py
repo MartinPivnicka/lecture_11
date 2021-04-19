6ab6f08977924bf00d50baefa84d3d67da4a2438
MartinPivnicka <pivnicka.martin@seznam.cz>
2021-04-19 10:56:04 +0200

    Add function linear_search

--- searching.py
+++ searching.py
@@ -22,9 +22,24 @@ def read_data(file_name, field):
     return data[field]
 
 
+def linear_search(sequence, number):
+    indices = []
+    count = 0
+    idx = 0
+    while idx < len(sequence):
+        if sequence[idx] == number:
+            count = count + 1
+            indices.append(idx)
+        idx = idx + 1
+
+    return {"positions": indices, "count": count}
+
+
 def main():
     sequential_data = read_data("sequential.json", "unordered_numbers")
-    print(sequential_data)
+    my_number = 10
+    results = linear_search(sequential_data, my_number)
+    print(results)
 
 
 if __name__ == '__main__':
